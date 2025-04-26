903aea39a2edcdb2742f7eb20b94a0b36a46766e
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-04-26 20:03:01 +0530

    Change in Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
+import Form1 from "./pages/Form1";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Dashboard />} />
+    //   </Routes>
+    // </Router>
+    <>
+    <Form1/>
+    </>
   );
 };
 
--- src/pages/Form1.jsx
+++ src/pages/Form1.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+
+import React, { useState } from "react";
 import "../styles/form1.css";
 
 const Form1 = () => {
