95b847afe6206b061497aeb82e73ac41e2d1433b
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-01 14:41:28 +0530

    Add Job Post Form feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Dashboard from './components/cvCreation.jsx';
+import JobPostForm from './components/JobPostForm.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Dashboard />
+    <JobPostForm />
   </StrictMode>
 );
