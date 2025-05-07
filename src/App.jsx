9d289d2e7dbca1e55cd4586dbe3cb7778aee94dd
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-05-07 18:24:54 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-// src/app.jsx
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Form1 from "./pages/Form1";
 
 const App = () => {
+
   return (
     // <Router>
     //   <Routes>
@@ -17,5 +18,10 @@ const App = () => {
     </>
   );
 };
+  
+
+
+
 
 export default App;
+
