69de1d1b1ad6943ddd1702b10a267155e23796f7
Heshani833 <heshanishehana123@gmail.com>
2025-05-13 23:34:34 +0530

    Registration_Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Form1 from "./pages/Form1";
-import Jobpostform from "./component/JobPostForm";
 
 const App = () => {
-
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Dashboard />} />
-    //   </Routes>
-    // </Router>
-    <>
-    <Form1/>
-    {/* <Dashboard/> */}
-    {/* <Jobpostform/> */}
-    </>
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Form1 />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 };
-  
-
-
-
 
 export default App;
-
