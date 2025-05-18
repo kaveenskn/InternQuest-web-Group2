4bf9e21a8eaca6cf8ef2b2dd97b1a0f8f80b3448
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-05-18 22:22:12 +0530

    .env file added

--- Server/configure/mongo.js
+++ Server/configure/mongo.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://InternQuestWeb:Internquestwebproject05@internquest.iydxuh3.mongodb.net/?retryWrites=true&w=majority&appName=InternQuest"
-    );
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
