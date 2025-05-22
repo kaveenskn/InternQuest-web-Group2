a3e3f62ab0f0f631f230eace3c99df3c32a69c9c
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-22 20:45:43 +0530

    Add InternshipFinder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import React from "react";
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./component/Dashboard";
-import Form1 from "./pages/Form1";
-import CardsSection from "./component/Cardsectionstd";
-import Jobpostform from "./component/JobPostForm";
-import signin from "./component/signin";
-import StudentDashboard from "./pages/StudentDashboard";
-import StudentProfile from "./pages/cvPage";
-import Login from "./pages/Login";
+import InternshipFinder from './component/InternshipFinder';
 
-const App = () => {
+function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Form1 />} />
-         <Route path="/job" element={<StudentDashboard/>} />
-         <Route path="/cv" element={<StudentProfile/>} />
-         <Route path="/login" element={<Login/>} />
-      </Routes>
-    </Router>
-
-
+    <div>
+      {/* ...other components */}
+      <InternshipFinder />
+    </div>
   );
-};
+}
 
-export default App;  
+export default App;
