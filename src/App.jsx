6d5a0ee89b079a5813556f44fe96951399c5310c
Heshani833 <heshanishehana123@gmail.com>
2025-05-24 21:22:50 +0530

    Change dashboard navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Form1 from "./pages/Form1";
-import Jobpostform from "./component/JobPostForm";
-import signin from "./component/signin";
+import JobPostForm from "./component/JobPostForm";
+import SignIn from "./component/Signin";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
 
 const App = () => {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Dashboard />} />
-    //   </Routes>
-    // </Router>
-    <>
-      <EmployeeDashboard />
-      {/* <Form1/> */}
-      {/* <Dashboard/> */}
-      {/* <Jobpostform />
-      <signin /> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<EmployeeDashboard />} />
+        <Route path="/form1" element={<Form1 />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jobpostform" element={<JobPostForm />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 };
 
