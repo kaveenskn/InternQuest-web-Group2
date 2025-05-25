cd08cb346d8bc28c8d36076f4aa2c5d92ed674e5
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-05-25 19:37:52 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import React from "react";
+import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Form1 from "./pages/Form1";
+import CardsSection from "./component/Cardsectionstd";
 import Jobpostform from "./component/JobPostForm";
 import signin from "./component/signin";
-import EmployeeDashboard from "./pages/EmployeeDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Dashboard />} />
-    //   </Routes>
-    // </Router>
-    <>
-      <EmployeeDashboard />
-      {/* <Form1/> */}
-      {/* <Dashboard/> */}
-      {/* <Jobpostform />
-      <signin /> */}
-    </>
+
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Form1 />} />
+         <Route path="/job" element={<StudentDashboard/>} />
+         <Route path="/login" element={<Login/>} />
+         <Route path="/post" element={<Jobpostform/>} />
+      </Routes>
+    </Router>
+
+
   );
 };
 
-export default App;
+export default App;  
