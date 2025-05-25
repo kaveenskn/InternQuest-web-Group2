1ded86e87fd595b2868aab7ccdf12506edd88834
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-05-25 20:24:52 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,6 @@ import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Form1 from "./pages/Form1";
-<<<<<<< HEAD
-import CardsSection from "./component/Cardsectionstd";
-import Jobpostform from "./component/JobPostForm";
-import signin from "./component/signin";
-import StudentDashboard from "./pages/StudentDashboard";
-import Login from "./pages/Login";
-
-const App = () => {
-  return (
-=======
 import JobPostForm from "./component/JobPostForm";
 import SignIn from "./component/Signin";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
@@ -30,16 +20,8 @@ const App = () => {
     </Router>
   );
 };
->>>>>>> 6d5a0ee89b079a5813556f44fe96951399c5310c
 
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Form1 />} />
-         <Route path="/job" element={<StudentDashboard/>} />
-         <Route path="/login" element={<Login/>} />
-         <Route path="/post" element={<Jobpostform/>} />
-      </Routes>
-    </Router>
-  );};
+
+  
 
 export default App; 
