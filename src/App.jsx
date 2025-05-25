a0254bb40eddde4740cc33088038db7437af6a36
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-25 11:30:18 +0530

    solve the errors in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./component/Dashboard";
 import Form1 from "./pages/Form1";
 import CardsSection from "./component/Cardsectionstd";
@@ -8,30 +9,26 @@ import Jobpostform from "./component/JobPostForm";
 import signin from "./component/signin";
 import StudentDashboard from "./pages/StudentDashboard";
 import Login from "./pages/Login";
-import CvForm from './components/CvForm';
-import InternshipFinder from './components/NewInternshipEntry';
-import JobPostform from './components/JobPostForm';
+
+import InternshipFinder from "./component/InternshipFinder";
 import LoginForm from "./component/LoginForm";
 import cvCreation from "./component/cvCreation";
 
 const App = () => {
   return (
-
     <Router>
       <Routes>
+        <Route path="/" element={<Dashboard />} /> {/* Default route */}
         <Route path="/signup" element={<Form1 />} />
-         <Route path="/job" element={<StudentDashboard/>} />
-         <Route path="/login" element={<Login/>} />
-         <Route path="/post" element={<Jobpostform/>} />
-
-         <Route path="/cv-form" element={<CvForm />} />
+        <Route path="/job" element={<StudentDashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post" element={<Jobpostform />} />
+        <Route path="/cv-form" element={<cvCreation />} />
         <Route path="/internships" element={<InternshipFinder />} />
         <Route path="/login-form" element={<LoginForm />} />
       </Routes>
     </Router>
-
-
   );
 };
 
-export default App;  
+export default App;
