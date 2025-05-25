301be27a6b72d2f358fd8cd8acfdeef44225fe9d
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-25 10:42:22 +0530

    import some files

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Jobpostform from "./component/JobPostForm";
 import signin from "./component/signin";
 import StudentDashboard from "./pages/StudentDashboard";
 import Login from "./pages/Login";
+import CvForm from './components/CvForm';
+import InternshipFinder from './components/NewInternshipEntry';
+import JobPostform from './components/JobPostForm';
+import LoginForm from "./component/LoginForm";
+import cvCreation from "./component/cvCreation";
 
 const App = () => {
   return (
@@ -18,6 +23,10 @@ const App = () => {
          <Route path="/job" element={<StudentDashboard/>} />
          <Route path="/login" element={<Login/>} />
          <Route path="/post" element={<Jobpostform/>} />
+
+         <Route path="/cv-form" element={<CvForm />} />
+        <Route path="/internships" element={<InternshipFinder />} />
+        <Route path="/login-form" element={<LoginForm />} />
       </Routes>
     </Router>
 
