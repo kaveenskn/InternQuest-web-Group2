105c248f570da9dc59cd46d659bd217e9325a64d
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-05-29 21:55:08 +0530

    added profile page for students

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import StudentDashboard from "./pages/StudentDashboard";
 import Login from "./pages/Login";
 import InternshipLandingPage from "./pages/InternshipLandingpage";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
+import ProfilePage from "./pages/profilePage";
+
+
 
 const App = () => {
   return (
@@ -18,6 +21,8 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/post" element={<Jobpostform />} />
         <Route path="/emp" element={<EmployeeDashboard />} />
+        <Route path="/profile" element={< ProfilePage/>} />
+      
       </Routes>
     </Router>
   );
