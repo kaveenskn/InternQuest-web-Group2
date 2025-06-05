4aa11a2a1b82a8df08ec0b75533f2b58dc7ab912
Heshani833 <heshanishehana123@gmail.com>
2025-06-05 19:38:47 +0530

    Add css file

---
+++ src/component/EmployeeFooter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/employeeFooter.css"; // Assuming you have a CSS file for styling
+
+const EmployeeFooter = () => {
+  return <div></div>;
+};
+
+export default EmployeeFooter;
