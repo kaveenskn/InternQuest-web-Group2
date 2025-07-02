ee96779710c836e874bfbace8070fb7c13892e4c
Heshani833 <heshanishehana123@gmail.com>
2025-07-02 16:02:18 +0530

    Change

--- src/component/EmHero.jsx
+++ src/component/EmHero.jsx
@@ -9,27 +9,27 @@ const EmHero = () => {
         <div className="hero-text">
           <h2 className="topic-text">Explore Our Services</h2>
         </div>
+      </div>
 
-        <p className="hero-description">
-          Explore our wide range of innovative services, carefully designed to
-          help you achieve your personal and professional goals. With a strong
-          focus on expert guidance, smart and efficient solutions, and a
-          commitment to personalized customer care, we are here to support you
-          every step of the way ensuring your success through innovation,
-          reliability, and excellence.
-        </p>
+      <p className="hero-description">
+        Explore our wide range of innovative services, carefully designed to
+        help you achieve your personal and professional goals. With a strong
+        focus on expert guidance, smart and efficient solutions, and a
+        commitment to personalized customer care, we are here to support you
+        every step of the way ensuring your success through innovation,
+        reliability, and excellence.
+      </p>
 
-        <div className="button-group">
-          <div className="hero-explore1">
-            <p>Get Started</p>
-          </div>
-          <div className="hero-explore2">
-            <p>Learn More</p>
-          </div>
+      <div className="button-group">
+        <div className="hero-explore1">
+          <p>Get Started</p>
         </div>
-        <div className="hero-image">
-          <img alt="hero-Img" src={employee} />{" "}
+        <div className="hero-explore2">
+          <p>Learn More</p>
         </div>
+      </div>
+      <div className="hero-image">
+        <img alt="hero-Img" src={employee} />{" "}
       </div>
     </div>
   );
