152de81e3202f7e00ec42ca8402550c82ddc00bd
Heshani833 <heshanishehana123@gmail.com>
2025-07-02 14:21:17 +0530

    Resolve errors

--- src/component/EmHero.jsx
+++ src/component/EmHero.jsx
@@ -18,30 +18,15 @@ const EmHero = () => {
           every step of the way ensuring your success through innovation,
           reliability, and excellence.
         </p>
+
         <div className="button-group">
           <div className="hero-explore1">
             <p>Get Started</p>
           </div>
           <div className="hero-explore2">
             <p>Learn More</p>
-          <h2 className="topic-text">Explore Our Services </h2>
-          <p className="hero-description">
-            Discover our innovative services designed to support your goals with
-            expert guidance, smart solutions and personalized customer care.
-          </p>
-          <div className="button-group">
-            <div className="hero-explore1">
-              <p>Get Started</p>
-          
-            </div>
-            <div className="hero-explore2">
-              <p>Learn More</p>
-              
-            </div>
           </div>
         </div>
-
-       
       </div>
     </div>
   );
