4bac0301b376ccf746f6e7197401ab74c4f8766b
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-07-14 10:38:37 +0530

    slider have changed

--- src/component/StudentsHome/ImageSlider.jsx
+++ src/component/StudentsHome/ImageSlider.jsx
@@ -1,47 +1,62 @@
-import React from 'react';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay } from 'swiper/modules';
+import { Pagination, Autoplay,EffectCoverflow, } from 'swiper/modules';
+import React, { useRef, useState } from 'react';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import "../../styles/ImageSlider.css";
 
 export default function ImageSlider() {
-  const pagination = {
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
-    },
-  };
+  
 
   return (
     <div className="slider-wrapper">
     <Swiper
-      pagination={pagination}
-      modules={[Pagination, Autoplay]}
-      autoplay={{ delay: 3000, disableOnInteraction: false }}
-      loop={true}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <img
-          src="https://www.usnews.com/object/image/00000191-80a0-d21f-a595-f3ed9f4d0000/gettyimages-2105091005.jpg?update-time=1724440047982&size=responsive640"
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src="https://assets.ascendientlearning.com/assets/CourseImages/2021-it-training-2__PadWzgwMCw0NTAsIkZGRkZGRiIsMF0_BGColorWzI1NSwyNTUsMjU1XQ.jpg"
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src="https://i.pinimg.com/736x/54/92/93/549293c9cb0b3bcd5874ad38d2a5fac2.jpg"
-          alt=""
-        />
-      </SwiperSlide>
-    </Swiper>
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+         initialSlide={2} 
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://cdn.leonardo.ai/users/bc687ae9-58de-4c67-ac57-345cde20882f/generations/897e8edd-718b-442e-b5fd-1e74c178c657/Leonardo_Phoenix_09_Create_an_image_for_InternQuest_to_showcas_2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.leonardo.ai/users/bc687ae9-58de-4c67-ac57-345cde20882f/generations/3d9cb71a-3a5b-4948-863b-cc6b8b38c157/Leonardo_Phoenix_09_Create_a_realistic_vibrant_and_modern_digi_3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.leonardo.ai/users/bc687ae9-58de-4c67-ac57-345cde20882f/generations/a3594c85-a7a5-4aba-9ccd-87e57a895555/Leonardo_Phoenix_09_Create_a_realistic_image_showcasing_the_In_2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.leonardo.ai/users/bc687ae9-58de-4c67-ac57-345cde20882f/generations/b2e087cd-b46a-4afd-aeb2-c3e5d804035b/Leonardo_Phoenix_09_Create_an_internquest_image_showcasing_men_2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.leonardo.ai/users/1e6ba8a7-4e01-405b-8f5a-a482ac2ebe67/generations/71c484cf-f662-4457-9b81-1118ab7136d7/segments/4:4:1/Flux_Dev_A_modern_banner_for_InternQuest_showing_a_profilebuil_3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://images.nightcafe.studio/jobs/mA98JOay7cK8YlrgHhNO/mA98JOay7cK8YlrgHhNO--0--ozkf0.jpg?tr=w-1600,c-at_max" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://images.nightcafe.studio/jobs/toYn9RNAwZoW4WSWySXt/toYn9RNAwZoW4WSWySXt--0--frjbu.jpg?tr=w-1600,c-at_max" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://images.nightcafe.studio/jobs/44hTRPI8NLcxsX8AhTx7/44hTRPI8NLcxsX8AhTx7--0--7nmq8.jpg?tr=w-1600,c-at_max" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://images.nightcafe.studio/jobs/8p2kp4RzZTFPZPmKsiQz/8p2kp4RzZTFPZPmKsiQz--0--j2ks7.jpg?tr=w-1600,c-at_max" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
